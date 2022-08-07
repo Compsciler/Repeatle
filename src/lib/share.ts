83886528e8b82e82912e8f2c3c00f634bdd67b79
Roger Wang <rogerwang2520@gmail.com>
2022-08-06 21:00:57 -0700

    Update shareable game link

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -8,7 +8,7 @@ const webShareApiDeviceTypes: string[] = ['mobile', 'smarttv', 'wearable']
 const parser = new UAParser()
 const browser = parser.getBrowser()
 const device = parser.getDevice()
-const gameUrl = 'rebrand.ly/repeatle'
+const gameUrl = 'repeatle.herokuapp.com'
 
 export const shareStatus = (
   solution: string,
